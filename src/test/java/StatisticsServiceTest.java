614a089bb31c77c4f40fbd5e69d5f551da3ad2c1
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2024-06-21 12:43:14 +0300

    Update StatisticsServiceTest.java

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -1,53 +1,76 @@
-package org.example;
+ppackage org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
-class RadioTest {
+public class RadioTest {
+    private Radio radio;
 
-    @Test
-    void shouldWrapAroundToFirstStationFromLast() {
-        Radio radio = new Radio();
-        radio.setStation(9); // Установка на последнюю станцию
-        radio.next(); // Переключение на следующую станцию
-        assertEquals(0, radio.getCurrentStation()); // Должно переключиться на первую станцию
+    @Before
+    public void setUp() {
+        radio = new Radio();
     }
 
     @Test
-    void shouldWrapAroundToLastStationFromFirst() {
-        Radio radio = new Radio();
-        radio.setStation(0); // Установка на первую станцию
-        radio.prev(); // Переключение на предыдущую станцию
-        assertEquals(9, radio.getCurrentStation()); // Должно переключиться на последнюю станцию
+    public void shouldStartAtStationZero() {
+        assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
-    void shouldIncreaseVolume() {
-        Radio radio = new Radio();
-        radio.increaseVolume(); // Увеличение громкости
-        assertEquals(51, radio.getCurrentVolume()); // Громкость должна увеличиться на 1
+    public void shouldStartAtMidVolume() {
+        assertEquals(50, radio.getCurrentVolume());
     }
 
     @Test
-    void shouldNotIncreaseVolumeAboveMax() {
-        Radio radio = new Radio();
-        for (int i = 0; i < 100; i++) {
-            radio.increaseVolume(); // Попытка увеличить громкость выше максимальной
+    public void shouldIncreaseStation() {
+        radio.next();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldDecreaseStation() {
+        radio.setStation(5);
+        radio.prev();
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldLoopToStationNineWhenPrevFromZero() {
+        radio.prev();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldLoopToStationZeroWhenNextFromNine() {
+        radio.setStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNotSetStationAboveNine() {
+        radio.setStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNotSetStationBelowZero() {
+        radio.setStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldIncreaseVolume() {
+        radio.increaseVolume();
+        assertEquals(51, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldNotIncreaseVolumeAboveMax() {
+        for (int i = 0; i < 51; i++) {
+            radio.increaseVolume();
         }
-        assertEquals(100, radio.getCurrentVolume()); // Громкость не должна превышать 100
-    }
-
-    @Test
-    void shouldSetStationWithinRange() {
-        Radio radio = new Radio();
-        radio.setStation(5); // Установка радиостанции в пределах допустимого диапазона
-        assertEquals(5, radio.getCurrentStation()); // Станция должна быть установлена
-    }
-
-    @Test
-    void shouldNotSetStationOutsideRange() {
-        Radio radio = new Radio();
-        radio.setStation(10); // Попытка установить станцию за пределами допустимого диапазона
-        assertEquals(0, radio.getCurrentStation()); // Станция не должна измениться
+        assertEquals(100, radio.getCurrentVolume());
     }
 }
